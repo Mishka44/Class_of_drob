8e3a4c46f2735a9b85e0ff3778105bd4c46cbaaf
Mikhail Biriukov <12212005b@gmail.com>
2023-10-01 22:13:47 +0300

    исправил НОД и функцию деления

--- Class_of_drob/Class_of_drob.cpp
+++ Class_of_drob/Class_of_drob.cpp
@@ -26,6 +26,8 @@ public:
             result.znam_ = (this->znam_ * (obj.znam_));
             result.chisl_ = (this->chisl_ * (obj.znam_)) + (obj.chisl_ * this->znam_);
         }
+        result.Reduction();
+        
         return result;
         
     }
@@ -40,12 +42,16 @@ public:
             result_of_sub.znam_ = (this->znam_ * (obj.znam_));
             result_of_sub.chisl_ = (this->chisl_ * (obj.znam_)) - (obj.chisl_ * this->znam_);
         }
+        result_of_sub.Reduction();
+        
         return result_of_sub;
     }
     Drob Multiplication(const Drob& obj) {
         Drob result_of_Mult;
         result_of_Mult.znam_ = this->znam_ * (obj.znam_);
         result_of_Mult.chisl_ = this->chisl_ * (obj.chisl_);
+        result_of_Mult.Reduction();
+       
         return result_of_Mult;
     }
     Drob Division(const Drob& obj) {
@@ -53,22 +59,22 @@ public:
         
         result_of_Div.znam_ = this->znam_ * (obj.chisl_);
         result_of_Div.chisl_ = this->chisl_ * (obj.znam_);
+        result_of_Div.Reduction();
+        
         return result_of_Div;
     }
-   /* int Nod() {
-        while (this->znam_!= 0) {
-            int t;
-            t = this->chisl_ % this->znam_;
-            this->chisl_ = this->znam_;
-            this->znam_ = t;
-        }
-        return this->chisl_;
-        
+    int Nod(int a, int b) {
+        a = abs(a), b = abs(b);
+        if (b == 0) return a;
+        else return Nod(b, a%b);
     }
-    void Reduction() {
-        this->chisl_ = this->chisl_ / Nod();
-        this->znam_ = this->znam_ / Nod();*/
+    void Reduction() { 
+        int a = Nod(this->chisl_, this->znam_);
+        this-> chisl_ = this->chisl_ / a;
+        this->znam_ = this->znam_ / a;
     }
+    
+
     
     
 
@@ -85,13 +91,14 @@ SetConsoleOutputCP(1251);
 setlocale(LC_ALL, "ru");
 Drob Number;
 Drob Number2;
-Number.chisl_and_znam(5, 2);
+Number.chisl_and_znam(5, 10);
 Number2.chisl_and_znam(1, 4);
 Number.show();
 Number = Number.Division(Number2);
+//Number.Reduction();
 //Number = Number.Subtraction(Number2);
 //Number = Number.Summ(Number2);
-//Number.Reduction();
+
 Number.show();
 
 }
